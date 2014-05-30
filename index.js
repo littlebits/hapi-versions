1482ab5aaa56871eb16f3bd9c7d5c078d25adac0
Jason Kuhrt <jasonkuhrt@me.com>
2014-05-29 23:06:01 -0400

    Release minor 0.1.0

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var ext_parse_api_version = require('./lib/uri-rewriter');
 
 module.exports = plugin;
 
-function plugin(server, versions){
-  server.ext('onRequest', ext_parse_api_version(versions));
+function plugin(server, versions, ignore){
+  server.ext('onRequest', ext_parse_api_version(versions, ignore));
 }
