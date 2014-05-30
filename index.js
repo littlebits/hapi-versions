a81d4852e38358df1058e621c0a8e8234ae3cde2
Jason Kuhrt <jasonkuhrt@me.com>
2014-05-29 23:05:35 -0400

    Remove table processor until more clarity around problem domain occurs

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 'use strict';
 
-var o = require('lodash');
 var ext_parse_api_version = require('./lib/uri-rewriter');
-var process_table = require('./lib/process-table');
 
 
 
 module.exports = plugin;
 
 function plugin(server, versions){
-  o.each(process_table(server.table(), versions), server.route.bind(server));
   server.ext('onRequest', ext_parse_api_version(versions));
 }
